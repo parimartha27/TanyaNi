ecd0ff3a777ac737d8471a34aff136d41e21454a
jessicahidayat <jessica.hidayat@binus.ac.id>
2022-11-26 14:57:17 +0700

    small change

--- website/views.py
+++ website/views.py
@@ -5,3 +5,11 @@ views = Blueprint('views',__name__)
 @views.route('/')
 def home():
   return render_template('home.html')
+
+@views.route('/errorPage')
+def errorPage():
+  return render_template('errorPage.html')
+
+@views.route('/notification')
+def notification():
+  return render_template('notification.html')
