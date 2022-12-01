60cd7e5f61b598eba2876346cc6fd2dde99f45d9
Kinethic <chandrakusuma228@icloud.com>
2022-12-01 13:48:59 +0700

    js

--- website/static/js/center.js
+++ website/static/js/center.js
@@ -15,3 +15,25 @@
         }, false)
       })
 })();
+
+const unReadMessages = document.querySelectorAll(".unread");
+const unRead = document.getElementById("notifications");
+const markAll = document.getElementById("mark_all");
+
+unread.innerText = unReadMessages.length;
+
+unReadMessages.forEach((message) => {
+    message.addEventListener("click", () => {
+        message.classList.remove("unread");
+        const newUnreadMessages = document.querySelectorAll(".unread");
+        unread.innerText = newUnreadMessages.length;
+    })
+})
+
+markAll.addEventListener("click", () => {
+    unReadMessages.forEach((message) => {
+        message.classList.remove("unread")
+    })
+    const newUnreadMessages = document.querySelectorAll(".unread");
+    unread.innerText = newUnreadMessages.length;
+})
